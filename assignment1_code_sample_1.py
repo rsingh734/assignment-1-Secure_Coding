23df00466922b0b36c67f0b3cc29c50301d51e21
Cordelia Usiokhale <cusiokhale@academic.rrc.ca>
2025-10-15 16:11:21 -0500

    added the vulnerability check

--- assignment1_code_sample_1.py
+++ assignment1_code_sample_1.py
@@ -1,12 +1,28 @@
+
+
 import os
 import pymysql
 from urllib.request import urlopen
+
+'''
+The vulnerabiity here is that, the database credentials is hard coded in the code.
+If an attacker gets access to this code then they have free access to the database.
+
+A07:2021 – Identification and Authentication Failures
+'''
 
 db_config = {
     'host': 'mydatabase.com',
     'user': 'admin',
     'password': 'secret123'
 }
+
+
+'''
+The vulnerability here is that we are taking the user input directly without validation and the user input may contain malious code.
+
+A03:2021 – Injection
+'''
 
 def get_user_input():
     user_input = input('Enter your name: ')
@@ -15,10 +31,22 @@ def get_user_input():
 def send_email(to, subject, body):
     os.system(f'echo {body} | mail -s "{subject}" {to}')
 
+'''
+The vulnerability here is that the external data is not validated
+
+A10:2021 – Server-Side Request Forgery (SSRF)
+'''
 def get_data():
     url = 'http://insecure-api.com/get-data'
     data = urlopen(url).read().decode()
     return data
+
+
+'''
+The vulnerabilty here is that the unvalidated data is stored in the database directly, which has exposed the database to SQL Injection.
+
+A03:2021 – Injection
+'''
 
 def save_to_db(data):
     query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
